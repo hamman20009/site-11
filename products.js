fd3c11add63585969a386940bab66c8b184f6275
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-05-18 22:49:51 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,18 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.png',
+		image3: 'img/products/3.png',
+		image4: 'img/products/4.png',
+		image5: 'img/products/5.png',
+		title: 'مرهم سوبلكسيبل للقضاء على الام الروماتيزم و لارتروز و عرق النساء  '  ,
+		price1: 4500,
+		price: 3800,
+		description: 'سيروم فيتوغان ضد تساقط الشعر يمنحك شعرا قويا و شادا حيث يقضي على الثعلبة و تساقط الشعر بسبب القلق  او حالات الصلع الوراثي   المنتج مجرب من طرف العديد من الازبناء ننصحك بتجريبه خاصة ة اان الكمية محدودة'  ,
+		phone:0554139163,
+	},
+
+	
+];
