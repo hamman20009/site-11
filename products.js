2fd1e9b0699bcd34c066bcbaf15f3502a66b4764
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-06-21 00:42:12 +0200

    Update products.js

--- products.js
+++ products.js
@@ -3,10 +3,7 @@ const products = [
 		id: 2,
 		logo:'img/logo.png',
 		image: 'img/products/1.png',
-		image2: 'img/products/2.png',
-		image3: 'img/products/3.png',
-		image4: 'img/products/4.png',
-		image5: 'img/products/5.png',
+		
 		title: 'مرهم سوبلكسيبل للقضاء على الام الروماتيزم و لارتروز و عرق النساء  '  ,
 		price1: 4500,
 		price: 3800,
