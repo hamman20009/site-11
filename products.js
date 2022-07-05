8e93b28301cec078b4f0df68bdc40d210798da59
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-07-05 13:42:07 +0200

    Update products.js

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ const products = [
 		logo:'img/logo.png',
 		image: 'img/1.png',
 		
-		title: 'اضواء لاد للمصابيح  الاميمية لسيارة  '  ,
+		title: 'اضواء لاد للمصابيح  الامامية لسيارة  '  ,
 		price1: 3500,
 		price: 2500,
 		description: '   '  ,
